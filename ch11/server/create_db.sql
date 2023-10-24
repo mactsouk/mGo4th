a08c9de2ec814fdb330390b6ecff729568478441
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2023-10-24 06:42:20 +0300

    Create tables for RESTful server

---
+++ ch11/server/create_db.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE users (
+    UserID INTEGER PRIMARY KEY,
+	username TEXT NOT NULL,
+	password TEXT NOT NULL,
+	lastlogin INTEGER,
+	admin INTEGER,
+	active INTEGER
+);
+
+
+INSERT INTO users (username, password, lastlogin, admin, active) VALUES ('admin', 'admin', 1620922454, 1, 1);
+
--- ch11/server/go.mod
+++ ch11/server/go.mod
@@ -6,5 +6,3 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/mattn/go-sqlite3 v1.14.17
 )
-
-require github.com/lib/pq v1.10.1 // indirect
