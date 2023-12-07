7a7c16394f3d962402e87314bc21503c6a76ed9a
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2023-12-07 20:56:33 +0200

    Writing tests for UNIX signals

--- ch12/testSignals/signalsTest.go
+++ ch12/testSignals/signalsTest.go
@@ -1,5 +1,40 @@
-package main
+package mySignals
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func HandleSignal(sig os.Signal) {
+	fmt.Println("handleSignal() Caught:", sig)
+}
+
+func Listener() {
+	fmt.Printf("Process ID: %d\n", os.Getpid())
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs)
+	start := time.Now()
+
+	go func() {
+		for {
+			sig := <-sigs
+			switch sig {
+			case syscall.SIGUSR1:
+				HandleSignal(sig)
+				duration := time.Since(start)
+				fmt.Println("Execution time:", duration)
+			case syscall.SIGUSR2:
+				HandleSignal(sig)
+			default:
+				fmt.Println("Caught:", sig)
+			}
+		}
+	}()
+
+	for {
+		time.Sleep(10 * time.Second)
+	}
 }
---
+++ ch12/testSignals/signalsTest_test.go
@@ -0,0 +1,34 @@
+package mySignals
+
+import (
+	"fmt"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func Test_All(t *testing.T) {
+	go Listener()
+	time.Sleep(time.Second)
+	test_SIGUSR1()
+	time.Sleep(time.Second)
+	test_SIGUSR2()
+	time.Sleep(time.Second)
+	test_SIGHUP()
+	time.Sleep(time.Second)
+}
+
+func test_SIGUSR1() {
+	fmt.Println("Sending syscall.SIGUSR1")
+	syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
+}
+
+func test_SIGUSR2() {
+	fmt.Println("Sending syscall.SIGUSR2")
+	syscall.Kill(syscall.Getpid(), syscall.SIGUSR2)
+}
+
+func test_SIGHUP() {
+	fmt.Println("Sending syscall.SIGHUP")
+	syscall.Kill(syscall.Getpid(), syscall.SIGHUP)
+}
