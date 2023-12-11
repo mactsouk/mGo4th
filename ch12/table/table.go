fb4cd103b9111b663654ffe00043dfce68b428f1
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2023-12-11 23:14:01 +0200

    Table driven tests

---
+++ ch12/table/table.go
@@ -0,0 +1,9 @@
+package division
+
+func intDiv(a, b int) int {
+	return a / b
+}
+
+func floatDiv(a, b int) float64 {
+	return float64(a) / float64(b)
+}
---
+++ ch12/table/table_test.go
@@ -0,0 +1,37 @@
+package division
+
+import (
+	"testing"
+)
+
+type myTest struct {
+	a        int
+	b        int
+	resInt   int
+	resFloat float64
+}
+
+var tests = []myTest{
+	{a: 1, b: 2, resInt: 0, resFloat: 0.5},
+	{a: 5, b: 10, resInt: 0, resFloat: 0.5},
+	{a: 2, b: 2, resInt: 1, resFloat: 1.0},
+	{a: 4, b: 2, resInt: 2, resFloat: 2.0},
+	{a: 5, b: 2, resInt: 2, resFloat: 2.5},
+	{a: 5, b: 4, resInt: 1, resFloat: 1.2},
+}
+
+func Test_all(t *testing.T) {
+	t.Parallel()
+
+	for _, test := range tests {
+		intResult := intDiv(test.a, test.b)
+		if intResult != test.resInt {
+			t.Fatalf("Expected %d, got %d", test.resInt, intResult)
+		}
+
+		floatResult := floatDiv(test.a, test.b)
+		if floatResult != test.resFloat {
+			t.Fatalf("Expected %f, got %f", test.resFloat, floatResult)
+		}
+	}
+}
