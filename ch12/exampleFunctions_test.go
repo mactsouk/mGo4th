d370e9a3707cd7d0f4dad9046ff5d3c14b7e4454
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2023-12-14 20:35:22 +0200

    Deleted duplicate directory

--- ch12/exampleFunctions_test.go
+++ ch12/exampleFunctions_test.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func ExampleLengthRange() {
 	fmt.Println(LengthRange("Mihalis"))
-	fmt.Println(LengthRange("Mastering Go, 3rd edition!"))
+	fmt.Println(LengthRange("Mastering Go, 4th edition!"))
 	// Output:
 	// 7
 	// 7
